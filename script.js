658fe95b0bd55b1bb04e9d5b45d4470d4ebe51cb
Mushroom1983 <151859550+Mushroom1983@users.noreply.github.com>
2024-03-07 14:32:29 +0000

    added code to JS and script file to HTML

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
 $(document).ready(function() {
-    console.log('Ready!');
-    
-    // Displaying the current date and time from: day.js
-    let now = dayjs().format('dddd, MMMM D, YYYY h:mm A');
-    let displayDate = document.getElementById('currentDay');
-    displayDate.innerHTML = now;
-  
-    let current = dayjs().format('HH');
-  
-    // past, present and future functionality to check the activity that happens in each time
-    $(".time-div").each(function(){
-      var timeDiv = $(this).data("hour");
-      if (current == timeDiv){
-        $(this).addClass('present');
-        $(this).children(".description").addClass("present");
-      } else if (current < timeDiv){
-        $(this).removeClass('present');
-        $(this).addClass('future');
-      } else {
-        $(this).removeClass('future');
-        $(this).addClass('past');
+    //Function to display current day at the top of the calendar
+    function displayCurrentDay() {
+        var currentDay = dayjs().format("dddd, MMMM D");
+        $("#currentDay").text("Today is " + currentDay);
       }
+    
+    //Function to generate time blocks for 9-5 busines hours
+    function generateTimeBlocks() {
+        var container = $(".container");
+        var currentHour = dayjs().hour();
+    
+        for (var hour = 9; hour <= 17; hour++) {
+          var timeBlock = $("<div>").addClass("time-block row");
+          var hourCol = $("<div>").addClass("hour col-md-1").text(hour + ":00");
+          var textAreaCol = $("<textarea>").addClass("description col-md-10");
+          var saveBtnCol = $("<button>").addClass("saveBtn col-md-1").text("Save");
+    
+          timeBlock.attr("data-hour", hour);
+          textAreaCol.attr("data-hour", hour);
+    
+    //Color-code time blocks based on past, present and future
+    if (hour < currentHour) {
+        textAreaCol.addClass("past");
+      } else if (hour === currentHour) {
+        textAreaCol.addClass("present");
+      } else {
+        textAreaCol.addClass("future");
+      }
+    
+      timeBlock.append(hourCol, textAreaCol, saveBtnCol);
+      container.append(timeBlock);
+    }
+}
+    //Function to handle click on save button and save event to local storage
+    function handleSaveButtonClick() {
+        $(".saveBtn").on("click", function() {
+          var hour = $(this).siblings(".description").data("hour");
+          var eventText = $(this).siblings(".description").val();
+    
+          // Save event in local storage
+          localStorage.setItem("event_" + hour, eventText);
+        });
+      }
+    
+    // Function to load events from local storage and populate time blocks
+    function loadEvents() {
+        $(".description").each(function() {
+          var hour = $(this).data("hour");
+          var storedEvent = localStorage.getItem("event_" + hour);
+    
+          if (storedEvent) {
+            $(this).val(storedEvent);
+          }
+        });
+      }
+    
+    // Call functions to initialize the planner
+    displayCurrentDay();
+    generateTimeBlocks();
+    handleSaveButtonClick();
+    loadEvents();
     });
-})
-
-// Creating function to save the data to the local storage
-$('.saveBtn').click(function(event) {
-    event.preventDefault();
-    var value = $(this).siblings('.time-block').val();
-    var time = $(this).parent().attr('id').split('-')[1];
-    localStorage.setItem(time, value);
-  });
+    
+    
